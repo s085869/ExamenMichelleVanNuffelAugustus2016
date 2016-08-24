ab4030205a8df038917059ae74dbd2a8fbb0a77e
Michelle Van Nuffel <michelle.vannuffel@student.ap.be>
2016-08-24 17:24:36 +0200

    Combinatie van kleine dingen
    Opdracht 1: reference error proberen oplossen (zonder resultaat)
    Opdracht 2: Recipe class gedeeltelijk gemaakt

--- ExamenOpdracht2/app/models/recipe.rb
+++ ExamenOpdracht2/app/models/recipe.rb
@@ -1,5 +1,24 @@
 class Recipe < ActiveRecord::Base
 
-  
+  def initialize(name, price, ingredients)
+    @recipeName=name
+    @recipePrice=price
+    @recipeIngredients=ingredients
+  end
 
+  def getName
+    "#@recipeName"
+  end
+
+  def getPrice
+    "#@recipePrice"
+  end
+
+  def getIngredients
+    "#@recipeIngredients"
+  end
+
+  def addIngredient
+    
+  end
 end
